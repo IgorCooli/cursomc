80f307cd3dab65ead4b4009728b9c601d82b9e56
Igor Oliveira <igorcooli@gmail.com>
2019-08-13 14:41:43 -0300

    [Add]_Categoria_Repository

--- src/main/java/com/iobeica/cursomc/repositories/CategoriaRepository.java
+++ src/main/java/com/iobeica/cursomc/repositories/CategoriaRepository.java
@@ -1,8 +1,13 @@
 package com.iobeica.cursomc.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.iobeica.cursomc.domain.Categoria;
+
 @Repository
-public interface CategoriaRepository  {
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+	
 
 }
