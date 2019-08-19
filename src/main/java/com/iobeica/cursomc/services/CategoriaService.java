0c3f517234872d187ab337c73523cd8fba11f786
Igor Oliveira <igorcooli@gmail.com>
2019-08-19 11:29:57 -0300

    [Add]_Deletando_Categoria_DELETE

--- src/main/java/com/iobeica/cursomc/services/CategoriaService.java
+++ src/main/java/com/iobeica/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.iobeica.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.iobeica.cursomc.domain.Categoria;
 import com.iobeica.cursomc.repositories.CategoriaRepository;
+import com.iobeica.cursomc.services.exceptions.DataIntegrityException;
 import com.iobeica.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir esta categoria pois ela possui produtos");
+		}
 	}
 }
